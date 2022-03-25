4138d091b91f47a5e9837f82a01664e284598927
tim.koch <tim.koch@es2000.de>
2022-03-25 11:15:42 +0200

    Function reponse

--- functions/submission-created.js
+++ functions/submission-created.js
@@ -3,14 +3,13 @@ const { EMAIL_TOKEN } = process.env
 const sgMail = require('@sendgrid/mail')
 const handler = async (event) => {
   const { name, message, email, number } = JSON.parse(event.body).payload.data
-  try {
-    sgMail.setApiKey(EMAIL_TOKEN)
-    const toRees = {
-      to: 'timmikoch@gmail.com',
-      from: 'Ree\'s Desk <tim@dezinerdudes.com>',
-      subject: "New message from Ree's Desk",
-      text: message,
-      html: `<p>Hey Marie,</p>
+  sgMail.setApiKey(EMAIL_TOKEN)
+  const toRees = {
+    to: 'timmikoch@gmail.com',
+    from: "Ree's Desk <tim@dezinerdudes.com>",
+    subject: "New message from Ree's Desk",
+    text: message,
+    html: `<p>Hey Marie,</p>
       <p>You received a new message from your site Ree's Desk. See details below:</p>
       <p>Client Name: ${name}<br>
       Email: ${email}<br>
@@ -19,12 +18,12 @@ const handler = async (event) => {
       </p>
       <p>Ree's Desk Site</p>
       `
-    }
-    const toClient = {
-      to: email,
-      from: 'ReesDesk <tim@dezinerdudes.com>',
-      subject: "Thank you for contacting Ree's Desk",
-      text: `Hey ${name},!
+  }
+  const toClient = {
+    to: email,
+    from: 'ReesDesk <tim@dezinerdudes.com>',
+    subject: "Thank you for contacting Ree's Desk",
+    text: `Hey ${name},!
       
       This is just to say we got your message and will be in touch with you shortly!
       
@@ -32,24 +31,28 @@ const handler = async (event) => {
       Ree's Desk Team
       `,
 
-      html: `<p>Hey there ${name},</p>
+    html: `<p>Hey there ${name},</p>
       <p>Thank you for reaching out to us!</p>
       <p>We have received your message and will get back to you shortly!<p>
       <p>Looking forward to chat!</p>
       <p>Kind Regards,<br>
       Ree's Desk Team</p>
       `
+  }
+  try {
+    const mailReponse = {
+      client: await sgMail.send(toClient),
+      rees: await sgMail.send(toRees)
     }
-    sgMail
-      .send(toClient).then(sgMail.send(toRees))
-      .then(() => {
-        return event
-      })
-      .catch((error) => {
-        return error
-      })
-  } catch (error) {
-    return { statusCode: 500, body: error.toString() }
+    return {
+      statusCode: 200,
+      body: JSON.stringify(mailReponse)
+    }
+  } catch (e) {
+    return {
+      statusCode: 500,
+      body: JSON.stringify(e)
+    }
   }
 }
 
